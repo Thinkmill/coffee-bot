20725c3f74c14a89e0afacc95802c5b8af2d29e0
Mike James <m@mikejam.es>
2017-07-28 12:00:33 +1000

    replying to a conversation

--- src/slackbot.js
+++ src/slackbot.js
@@ -3,14 +3,14 @@ var bot = slack.rtm.client();
 var token = process.env.SLACK_TOKEN;
 
 module.exports = () => {
-	// do something with the rtm.start payload
-	bot.started(function(payload) {
-		console.log('payload from rtm.start', payload);
-	});
-
-	// respond to a user_typing message
-	bot.user_typing(function(msg) {
-		console.log('several people are coding', msg);
+	bot.message(message => {
+		console.log('message', message);
+		if (message.subtype !== 'bot_message') {
+			slack.chat.postMessage(
+				{ text: 'hi', channel: message.channel, token },
+				console.log
+			);
+		}
 	});
 
 	// start listening to the slack team associated to the token
